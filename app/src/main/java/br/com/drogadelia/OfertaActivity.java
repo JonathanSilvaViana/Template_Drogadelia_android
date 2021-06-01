e2f23215599fb9991c7d10ba44081f729b0217b1
JonathanSilvaViana <jonathansilva@daum.net>
2021-06-01 18:20:49 -0300

    menu top bar completo

--- app/src/main/java/br/com/drogadelia/OfertaActivity.java
+++ app/src/main/java/br/com/drogadelia/OfertaActivity.java
@@ -2,14 +2,22 @@ package br.com.drogadelia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
+
 public class OfertaActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oferta);
+
+        setTitle(R.string.app_name);
+
 
 
     }
