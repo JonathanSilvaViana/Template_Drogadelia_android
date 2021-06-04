d180bedd9e84fdc0a208aece99b84e61911a7ccc
JonathanSilvaViana <jonathansilva@daum.net>
2021-06-04 11:55:19 -0300

    direções do slide prontas, falta apenas ajustes de layout

--- app/src/main/java/br/com/drogadelia/OfertaActivity.java
+++ app/src/main/java/br/com/drogadelia/OfertaActivity.java
@@ -13,11 +13,13 @@ import android.view.Display;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -29,7 +31,10 @@ public class OfertaActivity extends AppCompatActivity {
 
     Context context;
 
-    ImageButton carrinho_bt, updade_bt;
+    ImageButton carrinho_bt, voltar_slide, avancar_slide, voltar_slide_dois, voltar_slide_um, avancar_slide_dois, avancar_slide_tres;
+    Button bt_update_app;
+    String imgA, imgB, imgC;
+    int pegarID_img;
 
     final static int transicao = 2600;
     final static int transicaoII = 5200;
@@ -54,14 +59,69 @@ public class OfertaActivity extends AppCompatActivity {
 
         //executa a transição das imagens
 
+        //nome das imagens dos slides
+        imgA = "banner1";
+        imgB = "banner2";
+        imgC = "banner3";
+
         imgV = (ImageView)findViewById(R.id.imageSlider);
 
         //inicia o slide
-        autoslide();
+        //autoslide();
+
+        //encontra o botão para retroceder o slide
+        voltar_slide = (ImageButton)findViewById(R.id.voltar_slide);
+        voltar_slide_dois = (ImageButton)findViewById(R.id.voltar_slide_dois);
+        voltar_slide_um = (ImageButton)findViewById(R.id.voltar_slide_um);
+
+        //faz voltar o slide anterior
+        voltar_slide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retroce_slide_img_tres();
+            }
+        });
+
+        voltar_slide_dois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrocede_slide_img_dois();
+            }
+        });
+
+        voltar_slide_um.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrocede_slide_img_zero();
+            }
+        });
 
 
+        //encontra o botão para avançar o slide
+        avancar_slide = (ImageButton)findViewById(R.id.avancar_slide);
+        avancar_slide_dois = (ImageButton)findViewById(R.id.avancar_slide_dois);
+        avancar_slide_tres = (ImageButton)findViewById(R.id.avancar_slide_tres);
 
+        avancar_slide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                avanca_slide_dois();
+            }
+        });
 
+        avancar_slide_dois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Avancar_slide_tres();
+            }
+        });
+
+        avancar_slide_tres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                avanca_slide_img_zero();
+            }
+        });
 
     }
 
@@ -87,9 +147,79 @@ public class OfertaActivity extends AppCompatActivity {
             @Override
             public void run() {
                 imgV.setImageResource(R.drawable.banner1);
-
             }
         }, transicaoIII);
+    }
+
+    //nesses metodos pode ser necessário deixar os demais botões ocultos
+    public void retroce_slide_img_tres()
+    {
+
+        voltar_slide.setVisibility(View.GONE);
+        avancar_slide.setVisibility(View.GONE);
+        voltar_slide_dois.setVisibility(View.VISIBLE);
+        avancar_slide_tres.setVisibility(View.VISIBLE);
+        voltar_slide_dois.setBackgroundColor(getResources().getColor(R.color.black));
+        avancar_slide_tres.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner3);
+
+    }
+
+    public void retrocede_slide_img_dois()
+    {
+        voltar_slide_dois.setVisibility(View.GONE);
+        avancar_slide_tres.setVisibility(View.GONE);
+        voltar_slide_um.setVisibility(View.VISIBLE);
+        avancar_slide_dois.setVisibility(View.VISIBLE);
+        voltar_slide_um.setBackgroundColor(getResources().getColor(R.color.black));
+        avancar_slide_dois.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner2);
+    }
+
+    public void retrocede_slide_img_zero()
+    {
+        voltar_slide_um.setVisibility(View.GONE);
+        avancar_slide_dois.setVisibility(View.GONE);
+        voltar_slide.setVisibility(View.VISIBLE);
+        avancar_slide.setVisibility(View.VISIBLE);
+        voltar_slide.setBackgroundColor(getResources().getColor(R.color.black));
+        avancar_slide.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner1);
+    }
+
+    //métodos de avançar o slide
+
+    public void avanca_slide_dois()
+    {
+        avancar_slide.setVisibility(View.GONE);
+        voltar_slide.setVisibility(View.GONE);
+        avancar_slide_dois.setVisibility(View.VISIBLE);
+        voltar_slide_um.setVisibility(View.VISIBLE);
+        avancar_slide_dois.setBackgroundColor(getResources().getColor(R.color.black));
+        voltar_slide_um.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner2);
+    }
+
+    public void Avancar_slide_tres()
+    {
+        avancar_slide_dois.setVisibility(View.GONE);
+        voltar_slide_um.setVisibility(View.GONE);
+        avancar_slide_tres.setVisibility(View.VISIBLE);
+        voltar_slide_dois.setVisibility(View.VISIBLE);
+        avancar_slide_tres.setBackgroundColor(getResources().getColor(R.color.black));
+        voltar_slide_dois.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner3);
+    }
+
+    public void avanca_slide_img_zero()
+    {
+        avancar_slide_tres.setVisibility(View.GONE);
+        voltar_slide_dois.setVisibility(View.GONE);
+        avancar_slide.setVisibility(View.VISIBLE);
+        voltar_slide.setVisibility(View.VISIBLE);
+        avancar_slide_tres.setBackgroundColor(getResources().getColor(R.color.black));
+        voltar_slide_dois.setBackgroundColor(getResources().getColor(R.color.black));
+        imgV.setImageResource(R.drawable.banner1);
     }
 
 
